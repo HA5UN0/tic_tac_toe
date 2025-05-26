d4b5ffd445f3396a4dc7c642acb34dfa2ce52878
HA5UN0 <ha5un0xsama@gmail.com>
2025-05-26 09:45:05 -0400

    grid data is now being stored

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,5 +1,7 @@
 extends Node
 
+var player 
+var grid_data : Array
 var grid_pos : Vector2i
 var board_size : int
 var board_height : int
@@ -12,6 +14,7 @@ func _ready() -> void:
 	board_height = $Board.texture.get_height()
 	#print(board_size)
 	#print(board_height)
+	new_game()
 	
 	# divide board size by 3 to get the size of individual cell
 	cell_size = board_size / 3
@@ -29,5 +32,17 @@ func _input(event: InputEvent) -> void:
 			if event.position.x < board_size:
 				# conver mouse position into grid location
 				grid_pos = Vector2i(event.position / cell_size)
-				print(grid_pos)
+				# check first to see if anything is there
+				if grid_data[grid_pos.y][grid_pos.x] == 0:
+					grid_data[grid_pos.y][grid_pos.x] = player
+					player *= -1
+					print(grid_data)
 			
+
+func new_game():
+	player = 1
+	grid_data = [
+		[0,0,0],
+		[0,0,0],
+		[0,0,0]
+	]
